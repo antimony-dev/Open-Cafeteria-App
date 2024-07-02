7852623e72b0c86dc8b0b24ecee42daebf5e4d04
antimony-dev <shashw1013at@gmail.com>
2024-07-02 22:10:54 +0530

    items screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:oc/widgets/splash_screen.dart';
 
-<<<<<<< HEAD
 void main() => runApp(new MyApp());
-=======
-void main() {
-  runApp(MyApp());
-}
->>>>>>> Shashwat-Dev
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
